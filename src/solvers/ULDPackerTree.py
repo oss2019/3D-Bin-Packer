0551677be4eee62d29f18f72cb5fd1cd8d1cfa00
Vishvanathan K <vishvanathank@outlook.in>
2024-12-03 18:20:59 +0530

    Inserting and searching in Space Tree

--- src/solvers/ULDPackerTree.py
+++ src/solvers/ULDPackerTree.py
@@ -1,8 +1,10 @@
+from operator import truediv
 from typing import List, Tuple
 from dataclass.ULD import ULD
 from dataclass.Package import Package
 import numpy as np
 from .ULDPackerBase import ULDPackerBase
+from itertools import permutations
 
 
 class SpaceNode:
@@ -113,7 +115,6 @@ class SpaceNode:
 
         return updated_spaces
 
-
 class SpaceTree:
     def __init__(
         self,
@@ -122,7 +123,7 @@ class SpaceTree:
         self.uld_no = uld.id
         self.root = SpaceNode(np.zeros(3), uld.dimensions)
 
-    def divide_node_into_children(node_to_divide: SpaceNode, package: Package):
+    def divide_node_into_children(self,node_to_divide: SpaceNode, package: Package):
         if not node_to_divide.is_leaf:
             raise Exception("Dividing non leaf node")
 
@@ -230,6 +231,28 @@ class SpaceTree:
         else:
             raise Exception("Trying to pack package outside boundaries of space")
 
+    def search(self, package: Package, search_policy="bfs"):
+        volume = np.prod(package.rotation)
+
+        if search_policy.lower() == "bfs":
+            best_fit_vol = np.inf
+            best_fit_node = None
+
+            to_search = [self.root]
+            while to_search:
+                searching_node = to_search.pop(0)
+                if searching_node.is_leaf:
+                    if (np.prod(searching_node.dimensions) - volume > 0) and (np.prod(searching_node.dimensions) < best_fit_vol):
+                        for rot in permutations(package.dimensions):
+                            if rot[0] <= searching_node.dimensions[0] and rot[1] <= searching_node.dimensions[1] and rot[2] <= searching_node.dimensions[2]:
+                                best_fit_vol = np.prod(searching_node.dimensions)
+                                best_fit_node = searching_node
+                                package.rotation = rot
+                                break
+                to_search.extend(searching_node.children)
+            return (best_fit_node, best_fit_vol)
+        return None
+
     def display_tree(self, node=None, depth=0):
         """
         Display the space tree, including overlaps, for debugging purposes.
@@ -242,7 +265,6 @@ class SpaceTree:
 
         for child in node.children:
             self.display_tree(child, depth + 1)
-
 
 class ULDPackerBasicNonOverlap(ULDPackerBase):
     def __init__(
@@ -258,109 +280,85 @@ class ULDPackerBasicNonOverlap(ULDPackerBase):
             priority_spread_cost,
             max_passes,
         )
+        self.unpacked_packages = []
+        self.space_trees = [SpaceTree(u) for u in ulds]
 
-    def _find_available_space(
-        self, uld: ULD, package: Package
-    ) -> Tuple[bool, np.ndarray]:
-        length, width, height = package.dimensions
+    def insert(self, package: Package):
+        for st in self.space_trees:
+            space = st.search(package)
+            if space[0] != None:
+                st.divide_node_into_children(space[0],package)
+                return True
+        return False
 
-        # TODO CHANGE THIS
-        # for area in uld.available_spaces:
-        #     x, y, z, al, aw, ah = area
-        #     if length <= al and width <= aw and height <= ah:
-        #         return True, np.array([x, y, z])
-        # return False, None
 
-    def _update_available_spaces(
-        self, uld: ULD, position: np.ndarray, package: Package
-    ):
-        length, width, height = package.dimensions
-        x, y, z = position
 
-        updated_spaces = []
-        for space in uld.available_spaces:
-            ax, ay, az, al, aw, ah = space
-
-            # Check for remaining free areas after packing
-            if not (
-                x + length <= ax
-                or x >= ax + al
-                or y + width <= ay
-                or y >= ay + aw
-                or z + height <= az
-                or z >= az + ah
-            ):
-                if y + width <= ay + aw:
-                    updated_spaces.append(
-                        [ax, y + width, az, al, aw - (y + width - ay), ah]
-                    )
-                if y > ay:
-                    updated_spaces.append([ax, ay, az, al, y - ay, ah])
-                if x > ax:
-                    updated_spaces.append([ax, ay, az, x - ax, aw, ah])
-                if x + length <= ax + al:
-                    updated_spaces.append(
-                        [x + length, ay, az, al - (x + length - ax), aw, ah]
-                    )
-                if z > az:
-                    updated_spaces.append([ax, ay, az, al, aw, z - az])
-                if z + height <= az + ah:
-                    updated_spaces.append(
-                        [ax, ay, z + height, al, aw, ah - (z + height - az)]
-                    )
-            else:
-                updated_spaces.append(space)
-
-        uld.available_spaces = updated_spaces
-
-    def pack(self):
-        priority_packages = sorted(
-            [pkg for pkg in self.packages if pkg.is_priority],
-            key=lambda p: p.delay_cost,
-            reverse=True,
-        )
-        economy_packages = sorted(
-            [pkg for pkg in self.packages if not pkg.is_priority],
-            key=lambda p: p.delay_cost,
-            reverse=True,
-        )
-
-        # First pass - initial packing
-        for package in priority_packages + economy_packages:
-            packed = False
-            for uld in self.ulds:
-                if self._try_pack_package(package, uld):
-                    packed = True
-                    break
-            if not packed:
-                self.unpacked_packages.append(package)
-
-        # Multi-pass strategy to optimize packing
-        for pass_num in range(self.max_passes - 1):  # Exclude first pass
-            # Try to repack packages into available spaces
-            for package in self.unpacked_packages:
-                packed = False
-                for uld in self.ulds:
-                    if self._try_pack_package(package, uld):
-                        packed = True
-                        break
-                if packed:
-                    self.unpacked_packages.remove(package)
-
-        total_delay_cost = sum(pkg.delay_cost for pkg in self.unpacked_packages)
-        priority_spread_cost = self.priority_spread_cost * len(
-            {
-                uld_id
-                for _, uld_id, *_ in self.packed_positions
-                if any(p.id == _ and p.is_priority for p in self.packages)
-            }
-        )
-        total_cost = total_delay_cost + priority_spread_cost
-
-        return (
-            self.packed_positions,
-            self.packed_packages,
-            self.unpacked_packages,
-            self.uld_has_prio,
-            total_cost,
-        )
+    # def _find_available_space(
+    #     self, uld: ULD, package: Package
+    # ) -> Tuple[bool, np.ndarray]:
+    #     length, width, height = package.dimensions
+    #
+    #
+    #     # for area in uld.available_spaces:
+    #     #     x, y, z, al, aw, ah = area
+    #     #     if length <= al and width <= aw and height <= ah:
+    #     #         return True, np.array([x, y, z])
+    #     # return False, None
+    #
+    # def _update_available_spaces(
+    #     self, uld: ULD, position: np.ndarray, package: Package
+    # ):
+    #
+    #     pass
+    #
+    # def pack(self):
+    #     priority_packages = sorted(
+    #         [pkg for pkg in self.packages if pkg.is_priority],
+    #         key=lambda p: p.delay_cost,
+    #         reverse=True,
+    #     )
+    #     economy_packages = sorted(
+    #         [pkg for pkg in self.packages if not pkg.is_priority],
+    #         key=lambda p: p.delay_cost,
+    #         reverse=True,
+    #     )
+    #
+    #     # First pass - initial packing
+    #     for package in priority_packages + economy_packages:
+    #         packed = False
+    #         for uld in self.ulds:
+    #             if self._try_pack_package(package, uld):
+    #                 packed = True
+    #                 break
+    #         if not packed:
+    #             self.unpacked_packages.append(package)
+    #
+    #     # Multi-pass strategy to optimize packing
+    #     for pass_num in range(self.max_passes - 1):  # Exclude first pass
+    #         # Try to repack packages into available spaces
+    #         for package in self.unpacked_packages:
+    #             packed = False
+    #             for uld in self.ulds:
+    #                 if self._try_pack_package(package, uld):
+    #                     packed = True
+    #                     break
+    #             if packed:
+    #                 self.unpacked_packages.remove(package)
+    #
+    #     total_delay_cost = sum(pkg.delay_cost for pkg in self.unpacked_packages)
+    #     priority_spread_cost = self.priority_spread_cost * len(
+    #         {
+    #             uld_id
+    #             for _, uld_id, *_ in self.packed_positions
+    #             if any(p.id == _ and p.is_priority for p in self.packages)
+    #         }
+    #     )
+    #     total_cost = total_delay_cost + priority_spread_cost
+    #
+    #     return (
+    #         self.packed_positions,
+    #         self.packed_packages,
+    #         self.unpacked_packages,
+    #         self.uld_has_prio,
+    #         total_cost,
+    #     )
